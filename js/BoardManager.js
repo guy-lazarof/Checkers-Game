2ff36c5e6ed8c1b769f9c419981b7d34876a3fe0
Guy Lazarof <102980363+Guy-Lazarof@users.noreply.github.com>
2022-05-01 13:16:46 +0300

    on piece click+add class by ID

--- js/BoardManager.js
+++ js/BoardManager.js
@@ -18,7 +18,7 @@ class BoardManager {
         } else {
           cell.className = "light-cell";
         }
-        // cell.addEventListener("click", () => this.onPieceClick(i, j, cell.id));
+        cell.addEventListener("click", () => this.onPieceClick(i, j, cell.id));
       }
     }
   }
@@ -47,6 +47,45 @@ class BoardManager {
       this.Board[7][i - 1] = new Piece(7, i - 1, PAWN, `white`);
       this.Board[7][i - 1].createImage();
     }
-    console.log(this.Board);
+    // console.log(this.Board[0][1]);
+    console.log(this.Board[2][1].getPossibleMoves());
+  }
+
+  onPieceClick(row, col, cellID) {
+    //remove class to selected piece by ID
+    if (this.selectedPieceID !== undefined) {
+      document
+        .getElementById(this.selectedPieceID)
+        .classList.remove(`selected`);
+    }
+    //add class to selected piece- by ID
+
+    //remove class to possible moves by ID
+    if (this.filteredMovesByID !== undefined) {
+      for (let i = 0; i < this.filteredMovesByID.length; i++) {
+        document
+          .getElementById(this.filteredMovesByID[i])
+          .classList.remove(`possible-move`);
+      }
+    }
+    // get array of my possible moves and add class by ID
+    if (this.Board[row][col] !== undefined) {
+      document.getElementById(cellID).classList.add(`selected`);
+      this.selectedPieceID = cellID;
+      //array of filtered moves
+      this.filteredMoves = this.Board[row][col].getPossibleMoves();
+      console.log(this.filteredMoves);
+      // makes the possible moves array to a possible moves array by ID
+      this.filteredMovesByID = this.Board[row][col].Trans_To_Id_Cells();
+      for (let i = 0; i < this.filteredMovesByID.length; i++) {
+        document
+          .getElementById(this.filteredMovesByID[i])
+          .classList.add(`possible-move`);
+      }
+    }
+    // this.filteredMoves= the array of moves that evrey piece can do
+
+    //Add class to possible moves by ID
+    // console.log(this.Board[row][col].Trans_To_Id_Cells());
   }
 }
